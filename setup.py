6f4f57e42cde53650cab2170dae30cd2b4182d75
movciari <movciari@redhat.com>
2017-02-14 15:09:36 +0100

    Fix version

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from distutils.core import setup
 setup(
     name='simplecache',
     packages=['simplecache'],
-    version=0.1,
+    version='0.1',
     description='Simple caching mechanisms',
     author='Michal Ovciarik',
     author_email='movciari@gmail.com',
     url='https://github.com/bargulg/SimpleCache',
     download_url='https://github.com/bargulg/SimpleCache/tarball/0.1',
     keywords=['cache', 'caching', 'simple', 'easy'],
-    classifiers=[],
+    classifiers=[]
 )
