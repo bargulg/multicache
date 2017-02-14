e1a2cf02695a14a1edede1367fc4006ef84bcc3f
movciari <movciari@redhat.com>
2017-02-14 17:19:11 +0100

    Rename repo

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 
 setup(
-    name='simplecache',
-    packages=['simplecache'],
-    version='0.1',
+    name='multicache',
+    packages=['multicache'],
+    version='0.1.2',
     description='Simple caching mechanisms',
     author='Michal Ovciarik',
     author_email='movciari@gmail.com',
-    url='https://github.com/bargulg/SimpleCache',
-    download_url='https://github.com/bargulg/SimpleCache/tarball/0.1',
-    keywords=['cache', 'caching', 'simple', 'easy'],
+    url='https://github.com/bargulg/multicache',
+    download_url='https://github.com/bargulg/multicache/tarball/0.1',
+    keywords=['cache', 'caching', 'simple', 'easy', 'multi'],
     classifiers=[]
 )
